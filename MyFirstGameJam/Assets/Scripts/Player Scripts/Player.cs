5759a856720d9a42034921386cc559b03b24a4e2
qin-andrew123 <qin.andrew@wustl.edu>
2022-07-03 13:35:39 -0500

    Update

---
+++ MyFirstGameJam/Assets/Scripts/Player Scripts/Player.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Player : MonoBehaviour
+{
+    public Camera sceneCamera;
+    [SerializeField] float moveSpeed;
+
+    Rigidbody2D rb;
+    Vector2 moveDir;
+
+    private Vector2 mousePosition;
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+    }
+
+
+    private void Update()
+    {
+
+        float hor = Input.GetAxisRaw("Horizontal");
+        float ver = Input.GetAxisRaw("Vertical");
+        moveDir = new Vector2(hor, ver).normalized;
+
+
+
+
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = moveDir * moveSpeed;
+        processInput();
+        Move();
+    }
+
+    void processInput()
+    {
+        mousePosition = sceneCamera.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    private void Move()
+    {
+        Vector2 aimDirection = mousePosition - rb.position;
+        float aimAngel = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg + 90f;
+        rb.rotation = aimAngel;
+
+    }
+}
